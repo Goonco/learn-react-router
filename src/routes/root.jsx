f2e67eaf838311ae5c692dfb87dd405b3abe8d03
송경호 <s07019@naver.com>
2024-09-27 16:18:37 +0900

    :sparkles: synchronizing urls to form state

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,12 +1,13 @@
 import {Form, NavLink, Outlet, redirect, useLoaderData, useNavigation} from "react-router-dom";
 
 import {createContact, getContacts} from "../contacts";
+import {useEffect} from "react";
 
 export async function loader({request}) {
     const url = new URL(request.url);
     const q = url.searchParams.get("q");
     const contacts = await getContacts(q);
-    return {contacts};
+    return {contacts, q};
 }
 
 export async function action() {
@@ -15,8 +16,12 @@ export async function action() {
 }
 
 export default function Root() {
-    const {contacts} = useLoaderData()
+    const {contacts, q} = useLoaderData()
     const navigation = useNavigation()
+
+    useEffect(() => {
+        document.getElementById("q").value = q;
+    }, [q]);
 
     return (
         <>
@@ -31,6 +36,7 @@ export default function Root() {
                             placeholder="Search"
                             type="search"
                             name="q"
+                            defaultValue={q}
                         />
                         <div
                             id="search-spinner"
